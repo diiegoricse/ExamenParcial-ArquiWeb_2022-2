1a559df5af667a3287b672c1af428085e419f290
diiegoricse <u202016793@upc.edu.pe>
2022-10-04 19:13:41 -0500

    fix: data json

--- offers-app-202016793/data.js
+++ offers-app-202016793/data.js
@@ -2,32 +2,32 @@ module.exports = function () {
     var data = {
         Offers: [
             {
-              "id": 1,
-              "title": "Valentine's Days",
-              "description": "Buy on Valentine's Day with 25% discount in all shoes and get 80 Kinetic Points",
-              "points": 80,
-              "businessId": 1
+              id: 1,
+              title: "Valentine's Days",
+              description: "Buy on Valentine's Day with 25% discount in all shoes and get 80 Kinetic Points",
+              points: 80,
+              businessId: 1
             },
             {
-              "id": 2,
-              "title": "San Isidro Store",
-              "description": "Form a figure running by our San Isidro Store and get 30 Kinetic Points",
-              "points": 30,
-              "businessId": 2
+              id: 2,
+              title: "San Isidro Store",
+              description: "Form a figure running by our San Isidro Store and get 30 Kinetic Points",
+              points: 30,
+              businessId: 2
             },
             {
-              "id": 3,
-              "title": "At the Center",
-              "description": "Form a figure having at the center one of our stores locations and get 70 Kinetic Points",
-              "points": "71",
-              "businessId": 1
+              id: 3,
+              title: "At the Center",
+              description: "Form a figure having at the center one of our stores locations and get 70 Kinetic Points",
+              points: 71,
+              businessId: 1
             },
             {
-              "id": 4,
-              "title": "fcff",
-              "description": "cccdd",
-              "points": "42",
-              "businessId": "2"
+              id: 4,
+              title: "fcff",
+              description: "cccdd",
+              points: 42,
+              businessId: 2
             }
           ]
         
